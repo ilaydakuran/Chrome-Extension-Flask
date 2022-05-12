173139e666bb97e703d65d406f5ccb88ce640e06
ilaydakuran <ilaydakuran@sabanciuniv.edu>
2022-05-12 12:25:31 +0300

    small changes

--- app.py
+++ app.py
@@ -9,22 +9,27 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
-
+    return 'hellooo'
+    return render_template('/Users/ilaydakuran/Desktop/ext/options.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug= True
+    app.run()
 
-@app.route('/test', methods=['GET', 'POST']) # https://127.0.0.1:5000/test çalıştırıyoruz
-def testfn():
+@app.route('/api', methods=[ 'POST','GET']) # https://127.0.0.1:5000/test çalıştırıyoruz
+def met():
     # GET request
     if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!', 'timestamp':'ts', 'user_id':'123', 'metadata':{"smt":50} }
-        return jsonify(message)  # serialize and use JSON headers
+       message = {'message':"comment.txt", 'timestamp':'ts', 'user_id':' '}
+       print("succes")
+       return jsonify(message)  # serialize and use JSON headers
 
 
     # POST request
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
+        #name = request.form.get('name')
+        print(**request.get_json())  # parse as JSON
+        #message = {'greeting': 'Hello from Flask!', 'timestamp': 'ts', 'user_id': '123'}
+        #return jsonify(message)  # serialize and use JSON headers
         return 'Sucesss', 200
